c2e35e55e662d10529ddce20013f923706d61ed4
Pathak Vedant <vedantdpathak@iCloud.com>
2016-02-02 20:34:10 -0600

    Completed

--- array_of_challenges.rb
+++ array_of_challenges.rb
@@ -1,20 +1,24 @@
 #Line up Alphabetically 
 	#Sort all the names in this array alphabetically
 	star_students = [ "Kelly", "Henry", "Theo", "Jennifer", "Michelle"]
-
-puts star_students[2]
+	star_students.sort!
+	puts star_students[2]
 	#create a new array that has 19 elements of only your name as a string
 		#(hint: There is a way do this with only typing the string once)
-
-
+	myname = ["Vedant"]
+	18.times do
+		myname << "Vedant"
+	end
+	print myname
 	#That first array, star_students and this array, brilliant_students, should be one big array!
 		#combine this array with the one above
 		#Then sort the final array in alphabetic order
 
 	brilliant_students = ["Vedant", "Lena", "Zara", "Jessica"]
 
-	
-	#BONUS
+	students = star_students.concat brilliant_students
+	students.sort!
+	print students
 		#Iterate through the final student array, printing each student with an adjective
 			#ex:   Dynamic Dean   /n  Witty Andrew  /n  Diplomatic Luke
 
